d3427904542484c0b61d774110ded465ebc2d30d
Drew Wells <91301186+DrewWells178@users.noreply.github.com>
2023-03-28 12:00:42 -0400

    Rocket Launcher +
    Fully implemented the rocket launcher and cleaned up some files

--- My Game/Assets/RocketScript.cs
+++ My Game/Assets/RocketScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+
 public class RocketScript : MonoBehaviour
 {
     public Transform tip;
@@ -12,16 +13,19 @@ public class RocketScript : MonoBehaviour
     private Rigidbody2D rb;
     public float force;
 
-    public float speed = 20f;
+    public float speed = 3000f;
     public float rotateSpeed = 200f;
 
     public float damage = 30f;
+
+    private float splashRange = 1.5f;
 
     private Vector3 directions;
     
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Creating things");
         crosshair = GameObject.FindGameObjectWithTag("Crosshair").transform;
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
@@ -36,27 +40,58 @@ public class RocketScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //rotate and travel towards mouse position
-        Vector2 direction = (Vector2)crosshair.position - rb.position;
-        direction.Normalize();
-        //Vector2 face = (Vector2)tip.position - (Vector2)transform.position;
-        float rotateAmount = Vector3.Cross(direction, transform.up).z;
-        rb.angularVelocity = rotateAmount * rotateSpeed;
-        rb.velocity = -transform.up * speed;
+        rb.velocity = transform.right * force;
+            //rotate and travel towards mouse position
+        //Vector2 direction = (Vector2)crosshair.position - rb.position;
+        //direction.Normalize();
+            //Vector2 face = (Vector2)tip.position - (Vector2)transform.position;
+        //float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        //rb.angularVelocity = rotateAmount * rotateSpeed;
+        //rb.velocity = -transform.up * speed;
+        RotateRocket();
+    }
+
+    private void RotateRocket()
+    {
+        var heading = crosshair.position - transform.position;
+        var rotatedHeading = Quaternion.Euler(0,0,90) * heading;
+        var rotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: rotatedHeading);
+
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, speed * Time.deltaTime));
     }
 
     private void OnTriggerEnter2D (Collider2D hitInfo)
     {
-        Debug.Log(hitInfo.name);
+        //Debug.Log(hitInfo.name);
         
 
         if(hitInfo.name != "Player" && hitInfo.name != "Rocket(Clone)")
         {
-            Enemy enemy = hitInfo.GetComponent<Enemy>();
-            if(enemy != null)
+            if(splashRange > 0)
             {
-                enemy.TakeDamage(damage);
+                var hitColliders = Physics2D.OverlapCircleAll(transform.position, splashRange);
+                foreach(var hitCollider in hitColliders)
+                {
+                    Enemy enemy = hitCollider.GetComponent<Enemy>();
+                    if(enemy != null)
+                    {
+                        //var closestPoint = hitCollider.ClosestPoint(transform.position);
+                        //var distance = Vector3.Distance(closestPoint, transform.position);
+
+                        //var damagePercent = Mathf.InversLerp(splashRange, 0, distance);
+                        enemy.TakeDamage(damage);
+                    }
+                }
             }
+            else
+            {
+                Enemy enemy = hitInfo.GetComponent<Enemy>();
+                if(enemy != null)
+                {
+                    enemy.TakeDamage(damage);
+                }
+            }
+            
             Destroy(gameObject);
         }
     }
