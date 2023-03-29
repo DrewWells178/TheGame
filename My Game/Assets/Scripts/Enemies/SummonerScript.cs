6f2c4658a7c8cc0c02edd878327a2f3977e8f4e0
camrexwells <125075837+camrexwells@users.noreply.github.com>
2023-03-28 18:17:41 -0700

    Merge Prep

--- My Game/Assets/Scripts/Enemies/SummonerScript.cs
+++ My Game/Assets/Scripts/Enemies/SummonerScript.cs
@@ -7,7 +7,7 @@ using SVS.AIPlayerDetector;
 
 public class SummonerScript : Enemy
 {
-
+    /*
     private Rigidbody2D rb;
     private BoxCollider2D bc2;
     [SerializeField] LayerMask lm;
@@ -22,18 +22,34 @@ public class SummonerScript : Enemy
     public Transform[] patrolPoints;
     int currentPointIndex;
 
+    // Summoning Variables
+    public float timeBetweenSummons;
+    private float summonTime;
+    public Enemy enemyToSummon;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         bc2 = transform.GetComponent<BoxCollider2D>();
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (detector.PlayerDetected)
+        {
+            isPursuing = true;
+        }
+        if (!isPursuing)
+        {
+            Patrolling();
+        }
+        else
+        {
+            SummonerSpawn();
+        }
     }
     //Patrolling Function
     private void Patrolling()
@@ -58,4 +74,18 @@ public class SummonerScript : Enemy
 
         }
     }
+    private void SummonerSpawn()
+    {
+        if(Vector2.Distance(transform.position, player.position))
+    } else{
+    if(Time.time >= summonTime)
+        {
+         summonTime = Time.time + timeBetweenSummons;
+         // Set the animation trigger video #24 at 7:30
+        }
+        }
+//Summoning script
+Instantiate(enemyToSummon, transform.position, transform.rotation);
+
+    */
 }
